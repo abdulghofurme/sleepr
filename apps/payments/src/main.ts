192d9265acd5f3e6de95324911ae788777302284
Abdul Ghofur <abdulghofur.me@gmail.com>
2025-05-01 19:15:24 +0700

    feat: enhance payments module with configuration validation and dynamic port setup
    - Integrated ConfigModule for global configuration management in the payments module.
    - Added validation for HTTP_PORT using zod to ensure correct configuration.
    - Updated main application entry point to listen on the configured HTTP_PORT instead of a hardcoded value.

--- apps/payments/src/main.ts
+++ apps/payments/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { PaymentsModule } from './payments.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(PaymentsModule);
-  await app.listen(process.env.port ?? 3000);
+  const configService = app.get(ConfigService)
+  await app.listen(configService.get('HTTP_PORT'));
 }
 bootstrap();
--- apps/payments/src/payments.module.ts
+++ apps/payments/src/payments.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
 import { PaymentsController } from './payments.controller';
 import { PaymentsService } from './payments.service';
+import { ConfigModule } from '@nestjs/config';
+import { z } from 'zod';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validate: (config) => {
+        const parsed = z
+          .object({
+            HTTP_PORT: z.coerce.number(),
+          })
+          .safeParse(config);
+
+        if (!parsed.success) {
+          console.log('Config validation error:', parsed.error.format());
+          throw new Error('Invalid configuration');
+        }
+
+        return parsed.data;
+      },
+    }),
+  ],
   controllers: [PaymentsController],
   providers: [PaymentsService],
 })
