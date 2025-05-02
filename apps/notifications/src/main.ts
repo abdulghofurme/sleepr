1437d4009fdbac2b9a5a9e11badac7f2c93a7475
Abdul Ghofur <abdulghofur.me@gmail.com>
2025-05-02 18:47:34 +0700

    fix: correct Dockerfile and enhance notifications module with microservice support
    - Fixed the Dockerfile by correcting the pnpm-lock file name and the npm install command.
    - Updated main.ts to connect the notifications service to a TCP microservice with dynamic port configuration.
    - Integrated LoggerModule and ConfigModule in notifications.module.ts for improved logging and global configuration management.
    - Added configuration validation using zod to ensure correct TCP_PORT setup.

--- apps/notifications/src/main.ts
+++ apps/notifications/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { NotificationsModule } from './notifications.module';
+import { Logger } from 'nestjs-pino';
+import { ConfigService } from '@nestjs/config';
+import { Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(NotificationsModule);
-  await app.listen(process.env.port ?? 3000);
+  const configService = app.get(ConfigService);
+  app.connectMicroservice({
+    transport: Transport.TCP,
+    options: {
+      host: '0.0.0.0',
+      port: configService.get('TCP_PORT'),
+    },
+  });
+  app.useLogger(app.get(Logger));
+  await app.startAllMicroservices();
 }
 bootstrap();
--- apps/notifications/src/notifications.module.ts
+++ apps/notifications/src/notifications.module.ts
@@ -1,9 +1,31 @@
 import { Module } from '@nestjs/common';
 import { NotificationsController } from './notifications.controller';
 import { NotificationsService } from './notifications.service';
+import { LoggerModule } from '@app/common';
+import { ConfigModule } from '@nestjs/config';
+import { z } from 'zod';
 
 @Module({
-  imports: [],
+  imports: [
+    LoggerModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validate: (config) => {
+        const parsed = z
+          .object({
+            TCP_PORT: z.coerce.number(),
+          })
+          .safeParse(config);
+
+        if (!parsed.success) {
+          console.log('Config validation error:', parsed.error.format());
+          throw new Error('Invalid configuration');
+        }
+
+        return parsed.data;
+      },
+    }),
+  ],
   controllers: [NotificationsController],
   providers: [NotificationsService],
 })
