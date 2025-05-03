a6cd4deddca9c5deea1ce567fb79400ecdd1e365
Abdul Ghofur <abdulghofur.me@gmail.com>
2025-05-03 14:20:29 +0700

    feat: integrate MySQL support and refactor database module
    - Commented out MongoDB integration in favor of MySQL using TypeORM.
    - Updated Docker Compose to include MySQL service configuration.
    - Added MySQL environment variables for database connection.
    - Enhanced database module to support TypeORM for MySQL, including dynamic configuration based on environment variables.
    - Updated package.json and pnpm-lock.yaml to include necessary TypeORM and MySQL dependencies.

--- libs/common/src/database/database.module.ts
+++ libs/common/src/database/database.module.ts
@@ -1,20 +1,46 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { ModelDefinition, MongooseModule } from '@nestjs/mongoose';
+// MONGOOSE
+// import { ModelDefinition, MongooseModule } from '@nestjs/mongoose';
+
+// MYSQL
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { EntityClassOrSchema } from '@nestjs/typeorm/dist/interfaces/entity-class-or-schema.type';
 
 @Module({
   imports: [
-    MongooseModule.forRootAsync({
+    // MONGOOSE
+    // MongooseModule.forRootAsync({
+    //   imports: [ConfigModule],
+    //   useFactory: (configService: ConfigService) => ({
+    //     uri: configService.get('MONGODB_URI'),
+    //   }),
+    //   inject: [ConfigService],
+    // }),
+    // TYPEORM
+    TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       useFactory: (configService: ConfigService) => ({
-        uri: configService.get('MONGODB_URI'),
+        type: 'mysql',
+        host: configService.getOrThrow('MYSQL_HOST'),
+        port: configService.getOrThrow('MYSQL_PORT'),
+        database: configService.getOrThrow('MYSQL_DATABASE'),
+        username: configService.getOrThrow('MYSQL_USERNAME'),
+        password: configService.getOrThrow('MYSQL_PASSWORD'),
+        synchronize: configService.getOrThrow('MYSQL_SYNCHRONIZE'),
+        autoLoadEntities: true,
       }),
       inject: [ConfigService],
     }),
   ],
 })
 export class DatabaseModule {
-  static forFeature(models: ModelDefinition[]) {
-    return MongooseModule.forFeature(models);
+  // MONGOOSE
+  // static forFeature(models: ModelDefinition[]) {
+  // return MongooseModule.forFeature(models);
+  // }
+
+  static forFeature(models: EntityClassOrSchema[]) {
+    return TypeOrmModule.forFeature(models);
   }
 }
