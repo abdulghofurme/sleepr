896aeef82c19e2d600b6a64e559cb583928c3961
Abdul Ghofur <abdulghofur.me@gmail.com>
2025-05-07 11:02:30 +0700

    feat(graphql): initial feature by generate new app as gateway

--- apps/gateway/src/gateway.module.ts
+++ apps/gateway/src/gateway.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
-import { GatewayController } from './gateway.controller';
-import { GatewayService } from './gateway.service';
+import { ConfigModule } from '@nestjs/config';
+import { LoggerModule } from '@app/common';
+import { z } from 'zod';
 
 @Module({
-  imports: [],
-  controllers: [GatewayController],
-  providers: [GatewayService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validate: (config) => {
+        const parsed = z.object({
+          PORT: z.coerce.number(),
+        }).safeParse(config)
+
+        if (!parsed.success) {
+          console.log('Config validation error: ', parsed.error.format())
+          throw new Error('Invalid configuration')
+        }
+
+        return parsed.data
+      }
+    }),
+    LoggerModule
+  ],
+  controllers: [],
+  providers: [],
 })
 export class GatewayModule {}
