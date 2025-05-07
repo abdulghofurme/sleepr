d78699ac9e9b6ad38f1b01ef8ac154055dafe6bc
Abdul Ghofur <abdulghofur.me@gmail.com>
2025-05-07 11:41:17 +0700

    feat(graphql): setup gateway & include reservation as one of subgraphs
    RESERVATIONS_GRAPHQL_URL=http://reservations:3000/graphql

--- apps/gateway/src/gateway.module.ts
+++ apps/gateway/src/gateway.module.ts
@@ -1,15 +1,20 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { LoggerModule } from '@app/common';
 import { z } from 'zod';
+import { GraphQLModule } from '@nestjs/graphql'
+import { ApolloGatewayDriver, ApolloGatewayDriverConfig } from '@nestjs/apollo'
+import { IntrospectAndCompose } from '@apollo/gateway';
 
 @Module({
   imports: [
+    LoggerModule,
     ConfigModule.forRoot({
       isGlobal: true,
       validate: (config) => {
         const parsed = z.object({
           PORT: z.coerce.number(),
+          RESERVATIONS_GRAPHQL_URL: z.string().url()
         }).safeParse(config)
 
         if (!parsed.success) {
@@ -20,9 +25,24 @@ import { z } from 'zod';
         return parsed.data
       }
     }),
-    LoggerModule
+    GraphQLModule.forRootAsync<ApolloGatewayDriverConfig>({
+      driver: ApolloGatewayDriver,
+      useFactory: (configService: ConfigService) => ({
+        gateway: {
+          supergraphSdl: new IntrospectAndCompose({
+            subgraphs: [
+              {
+                name: "reservations",
+                url: configService.getOrThrow('RESERVATIONS_GRAPHQL_URL')
+              }
+            ]
+          })
+        }
+      }),
+      inject: [ConfigService]
+    })
   ],
   controllers: [],
   providers: [],
 })
-export class GatewayModule {}
+export class GatewayModule { }
